434b31a752d080c25242675f0e37f4fd73d03c99
Hattah10 <norhata.sampaco@g.msuiit.edu.ph>
2024-12-31 11:46:59 +0800

    add stack card animation

--- frontend/src/component/StackCard.jsx
+++ frontend/src/component/StackCard.jsx
@@ -1,22 +1,26 @@
 import React from "react";
 import StackCardLink from "./StackCardLink";
 import ToolTag from "./ToolTag";
+import { useTransform, motion } from "motion/react";
 
-export default function StackCard(props) {
+export default function StackCard({ props, i, progress, range, target }) {
+  const scale = useTransform(progress, range, [1, target]);
+
   return (
-    <div
-      key={props.title}
-      className="lg:h-screen flex  justify-center items-center "
+    <motion.div
+      style={{ top: `calc(100px + ${i * 25}px)`, scale }}
+      className="lg:h-dvh   z-20 sticky  xs:top-12 "
     >
+      {" "}
       <div className="flex flex-col lg:flex-row rounded-3xl shadow-2xl  w-full h-auto lg:h-[450px]">
-        <div className="w-full lg:w-1/2 h-[300px] lg:h-full  ">
+        <div className="w-full lg:w-1/2 h-[300px] lg:h-full hidden  md:block">
           <img
             src={props.image}
             alt=""
             className="w-full h-full object-cover rounded-tl-3xl lg:rounded-tr-none rounded-tr-3xl lg:rounded-s-3xl"
           />
         </div>
-        <div className="flex-1 w-full px-5 py-10 relative">
+        <div className="flex-1 w-full px-5 py-10 relative bg-white">
           <h1 className="text-3xl font-bold font-montserrat">
             {props.title}{" "}
             <span className="font-normal text-xl text-gray-500">
@@ -38,6 +42,6 @@ export default function StackCard(props) {
           </div>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 }
---
+++ frontend/src/data/Project.jsx
@@ -0,0 +1,73 @@
+import profile from "../assets/images/profile.jpg";
+
+export const recentProject = [
+  {
+    title: "POS Inventory System",
+    client: "GPIH",
+    role: "FullStack Developer",
+    description:
+      "Developed a real-time collaboration platform enabling seamless communication and data sharing. The project included features like live chat using Socket.IO, a responsive UI built with React and Tailwind, and integration with third-party APIs to enhance functionality.",
+    tools: ["react", "tailwind", "API", "socket.io"],
+    image: profile,
+    github: "Http",
+    externalLink: "",
+  },
+  {
+    title: "Online Job System",
+    client: "Pyschology Student",
+    role: "FullStack Developer",
+    description:
+      "Developed a real-time collaboration platform enabling seamless communication and data sharing. The project included features like live chat using Socket.IO, a responsive UI built with React and Tailwind, and integration with third-party APIs to enhance functionality.",
+    tools: ["react", "tailwind", "API", "socket.io"],
+    image: profile,
+    github: "Http",
+    externalLink: ".",
+  },
+  {
+    title: "POS Inventory System",
+    client: "GPIH",
+    role: "FullStack Developer",
+    description:
+      "Developed a real-time collaboration platform enabling seamless communication and data sharing. The project included features like live chat using Socket.IO, a responsive UI built with React and Tailwind, and integration with third-party APIs to enhance functionality.",
+    tools: ["react", "tailwind", "API", "socket.io"],
+    image: profile,
+    github: "Http",
+    externalLink: "",
+  },
+  {
+    title: "POS Inventory System",
+    client: "GPIH",
+    role: "FullStack Developer",
+    description:
+      "Developed a real-time collaboration platform enabling seamless communication and data sharing. The project included features like live chat using Socket.IO, a responsive UI built with React and Tailwind, and integration with third-party APIs to enhance functionality.",
+    tools: ["react", "tailwind", "API", "socket.io"],
+    image: profile,
+    github: "Http",
+    externalLink: "",
+  },
+];
+
+export const academicProject = [
+  {
+    title: "POS Inventory System",
+    client: "GPIH",
+    role: "FullStack Developer",
+    description:
+      "Developed a real-time collaboration platform enabling seamless communication and data sharing. The project included features like live chat using Socket.IO, a responsive UI built with React and Tailwind, and integration with third-party APIs to enhance functionality.",
+    tools: ["react", "tailwind", "API", "socket.io"],
+    image: profile,
+    github: "Http",
+    externalLink: "",
+  },
+  {
+    title: "Online Job System",
+    client: "Pyschology Student",
+    role: "FullStack Developer",
+    description:
+      "Developed a real-time collaboration platform enabling seamless communication and data sharing. The project included features like live chat using Socket.IO, a responsive UI built with React and Tailwind, and integration with third-party APIs to enhance functionality.",
+    tools: ["react", "tailwind", "API", "socket.io"],
+    image: profile,
+    github: "Http",
+    externalLink: ".",
+  },
+];
